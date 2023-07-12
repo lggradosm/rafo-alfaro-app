571379131f8e873de91c4a55c1fb32471202cc14
lggradosm <gabrielgradosmachado@gmail.com>
2023-07-11 21:10:49 -0500

    casa

--- src/util/equipoDummy.js
+++ src/util/equipoDummy.js
@@ -1,107 +1,109 @@
+const url = import.meta.env.BASE_URL;
+
 const equipoDummy = [
-    {
-        id:1,
-        name: "Marykarly Reyes",
-        position: "Jefe de Proyectos de Interiorismo",
-        image: "/tmp/equipo/Marykarly-Reyes.jpg"
-    },
+  {
+    id: 1,
+    name: "Marykarly Reyes",
+    position: "Jefe de Proyectos de Interiorismo",
+    image: `${url}/tmp/equipo/Marykarly-Reyes.jpg`,
+  },
 
   {
-        id:2,
-        name: "Gean Horna",
-        position: "Diseñador de Interiores",
-        image: "/tmp/equipo/Gean-Horna.jpg"
-    },
-    {
-        id:3,
-        name: "Anthony Elías",
-        position: "Diseñador de Interiores",
-        image: "/tmp/equipo/Anthony-Elias.jpg"
-    },
-    {
-        id:4,
-        name: "Diego Uriol",
-        position: "Jefe de Proyectos de Arquitectura",
-        image: "/tmp/equipo/Diego-Uriol.jpg"
-    },
-    {
-        id:5,
-        name: "Carlos Zapata",
-        position: "Arquitecto",
-        image: "/tmp/equipo/Carlos-Zapata.jpg"
-    },
-    {
-        id:6,
-        name: "Johan Ruíz",
-        position: "Arquitecto",
-        image: "/tmp/equipo/Johan-Ruiz.jpg"
-    },
-    {
-        id:7,
-        name: "Carlos Castañeda",
-        position: "Arquitecto",
-        image: "/tmp/equipo/Carlos-Castaneda.jpg"
-    },
-    {
-        id:8,
-        name: "César Toribio",
-        position: "Asistente de Diseño",
-        image: "/tmp/equipo/Cesar-Toribio.jpg"
-    },
-    {
-        id:9,
-        name: "Leidy Mendoza",
-        position: "Arquitecta",
-        image: "/tmp/equipo/Leidy-Mendoza.jpg"
-    },
-    {
-        id:10,
-        name: "Laura Dávila",
-        position: "Asistente de Diseño",
-        image: "/tmp/equipo/Laura-Davila.jpg"
-    },
-    {
-        id:11,
-        name: "Gabriel Grados",
-        position: "Visualizador Arquitectónico",
-        image: "/tmp/equipo/Luis-Grados.jpg"
-    },
-    {
-        id:12,
-        name: "Victor Ríos",
-        position: "Asistente de Ingeniería",
-        image: "/tmp/equipo/Victor-Rios.jpg"
-    },
-    {
-        id:13,
-        name: "Diego Paredes",
-        position: "Ingeniero Civil",
-        image: "/tmp/equipo/Diego-Paredes.jpg"
-    },
-    {
-        id:14,
-        name: "Ana Paula Ludeña",
-        position: "Arquitecta",
-        image: "/tmp/equipo/Ana-Paula-Ludena.jpg"
-    },
-    {
-        id:15,
-        name: "Ghelenn Sánchez",
-        position: "Ingeniera Civil",
-        image: "/tmp/equipo/Ghelenn-Sanchez.jpg"
-    },
-    {
-        id:16,
-        name: "Jhon Martell",
-        position: "Asistente de Supervisión",
-        image: "/tmp/equipo/Jhon-Martell.jpg"
-    },
-    {
-        id:17,
-        name: "Yelina Castro",
-        position: "Asistente de Ingeniería",
-        image: "/tmp/equipo/Yelina-Castro.jpg"
-    },
-]
+    id: 2,
+    name: "Gean Horna",
+    position: "Diseñador de Interiores",
+    image: `${url}/tmp/equipo/Gean-Horna.jpg`,
+  },
+  {
+    id: 3,
+    name: "Anthony Elías",
+    position: "Diseñador de Interiores",
+    image: `${url}/tmp/equipo/Anthony-Elias.jpg`,
+  },
+  {
+    id: 4,
+    name: "Diego Uriol",
+    position: "Jefe de Proyectos de Arquitectura",
+    image: `${url}/tmp/equipo/Diego-Uriol.jpg`,
+  },
+  {
+    id: 5,
+    name: "Carlos Zapata",
+    position: "Arquitecto",
+    image: `${url}/tmp/equipo/Carlos-Zapata.jpg`,
+  },
+  {
+    id: 6,
+    name: "Johan Ruíz",
+    position: "Arquitecto",
+    image: `${url}/tmp/equipo/Johan-Ruiz.jpg`,
+  },
+  {
+    id: 7,
+    name: "Carlos Castañeda",
+    position: "Arquitecto",
+    image: `${url}/tmp/equipo/Carlos-Castaneda.jpg`,
+  },
+  {
+    id: 8,
+    name: "César Toribio",
+    position: "Asistente de Diseño",
+    image: `${url}/tmp/equipo/Cesar-Toribio.jpg`,
+  },
+  {
+    id: 9,
+    name: "Leidy Mendoza",
+    position: "Arquitecta",
+    image: `${url}/tmp/equipo/Leidy-Mendoza.jpg`,
+  },
+  {
+    id: 10,
+    name: "Laura Dávila",
+    position: "Asistente de Diseño",
+    image: `${url}/tmp/equipo/Laura-Davila.jpg`,
+  },
+  {
+    id: 11,
+    name: "Gabriel Grados",
+    position: "Visualizador Arquitectónico",
+    image: `${url}/tmp/equipo/Luis-Grados.jpg`,
+  },
+  {
+    id: 12,
+    name: "Victor Ríos",
+    position: "Asistente de Ingeniería",
+    image: `${url}/tmp/equipo/Victor-Rios.jpg`,
+  },
+  {
+    id: 13,
+    name: "Diego Paredes",
+    position: "Ingeniero Civil",
+    image: `${url}/tmp/equipo/Diego-Paredes.jpg`,
+  },
+  {
+    id: 14,
+    name: "Ana Paula Ludeña",
+    position: "Arquitecta",
+    image: `${url}/tmp/equipo/Ana-Paula-Ludena.jpg`,
+  },
+  {
+    id: 15,
+    name: "Ghelenn Sánchez",
+    position: "Ingeniera Civil",
+    image: `${url}/tmp/equipo/Ghelenn-Sanchez.jpg`,
+  },
+  {
+    id: 16,
+    name: "Jhon Martell",
+    position: "Asistente de Supervisión",
+    image: `${url}/tmp/equipo/Jhon-Martell.jpg`,
+  },
+  {
+    id: 17,
+    name: "Yelina Castro",
+    position: "Asistente de Ingeniería",
+    image: `${url}/tmp/equipo/Yelina-Castro.jpg`,
+  },
+];
 
-export default equipoDummy
+export default equipoDummy;
